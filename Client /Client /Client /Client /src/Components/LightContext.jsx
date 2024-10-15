dc1afa2cf04ab2f05b74b3457473043ce67c6ecf
“Poova <poovasokan@gmail.com>
2024-10-15 08:33:31 +0100

    vel

---
+++ Client /Client /Client /Client /src/Components/LightContext.jsx
@@ -0,0 +1,165 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { createContext, useState } from "react";
+
+export const LightContext = createContext();
+
+// LightProvider component
+export const LightProvider = ({ children }) => {
+  const maxLightsPerType = 5;
+
+  const [lights, setLights] = useState([
+    { id: 1, type: "AMBIENT", name: "Ambient Light", intensity: 0.5 },
+    {
+      id: 2,
+      type: "DIRECTIONAL",
+      name: "Directional Light",
+      intensity: 0.8,
+      position: { x: 0, y: 5, z: 5 },
+      // castShadow: true,
+      bias: -0.005,
+    },
+    {
+      id: 3,
+      type: "HEMISPHERE",
+      name: "Hemisphere Light",
+      intensity: 0.6,
+      color: "#ffffff",
+      groundColor: "#0000ff",
+    },
+    {
+      id: 4,
+      type: "SPOT",
+      name: "Spot Light 1",
+      intensity: 0.7,
+      position: { x: 2, y: 5, z: 2 },
+      angle: 0.3,
+      decay: 2,
+      castShadow: true,
+    },
+    {
+      id: 5,
+      type: "SPOT",
+      name: "Spot Light 2",
+      intensity: 0.7,
+      position: { x: -2, y: 5, z: -2 },
+      angle: 0.3,
+      decay: 2,
+      castShadow: true,
+    },
+  ]);
+
+  // Function to update a light's settings by id
+  const updateLight = (id, newSettings) => {
+    setLights((prevLights) =>
+      prevLights.map((light) =>
+        light.id === id ? { ...light, ...newSettings } : light
+      )
+    );
+  };
+
+  // Function to delete a light by id
+  const deleteLight = (id) => {
+    setLights((prevLights) => prevLights.filter((light) => light.id !== id));
+  };
+
+  // Function to add a new light
+  const addLight = (newLight) => {
+    const newLightId = lights.length + 1;
+
+    const typeCount = lights.filter((l) => l.type === newLight.type).length;
+    if (typeCount >= maxLightsPerType) {
+      console.error(
+        `Maximum ${maxLightsPerType} lights allowed for ${newLight.type}`
+      );
+      return;
+    }
+
+    const lightSettings = {
+      AMBIENT: { intensity: 0.5 },
+      DIRECTIONAL: {
+        intensity: 1,
+        position: { x: 0, y: 5, z: 0 },
+        castShadow: false,
+        bias: -0.005,
+      },
+      HEMISPHERE: {
+        intensity: 0.6,
+        color: "#ffffff",
+        groundColor: "#0000ff",
+      },
+      SPOT: {
+        intensity: 0.7,
+        position: { x: 2, y: 5, z: 2 },
+        angle: 0.3,
+        decay: 2,
+        castShadow: true,
+      },
+    };
+
+    const lightType = newLight.type.toUpperCase();
+
+    if (!lightSettings[lightType]) {
+      console.error("Invalid light type:", lightType);
+      return;
+    }
+
+    const newLightWithSettings = {
+      id: newLightId,
+      type: lightType,
+      name: newLight.name,
+      ...lightSettings[lightType],
+    };
+
+    setLights((prevLights) => [...prevLights, newLightWithSettings]);
+  };
+
+  // Function to rename a light
+  const renameLight = (id, newName) => {
+    setLights((prevLights) =>
+      prevLights.map((light) =>
+        light.id === id ? { ...light, name: newName } : light
+      )
+    );
+  };
+
+  // Function to duplicate a light
+  const duplicateLight = (id) => {
+    const lightToDuplicate = lights.find((light) => light.id === id);
+    if (!lightToDuplicate) return;
+
+    const typeCount = lights.filter(
+      (l) => l.type === lightToDuplicate.type
+    ).length;
+    if (typeCount >= maxLightsPerType) {
+      console.error(
+        `Maximum ${maxLightsPerType} lights allowed for ${lightToDuplicate.type}`
+      );
+      return;
+    }
+
+    const newLight = {
+      ...lightToDuplicate,
+      id: lights.length + 1,
+      name: `${lightToDuplicate.name} (Copy)`,
+    };
+    setLights((prevLights) => [...prevLights, newLight]);
+  };
+
+  return (
+    <LightContext.Provider
+      value={{
+        lights,
+        updateLight,
+        deleteLight,
+        addLight,
+        renameLight,
+        duplicateLight,
+      }}
+    >
+      {children}
+    </LightContext.Provider>
+  );
+};
+//v3 - All UI updated
+//v4 - with UI with figma
---
+++ Client /Client /Client /Client /src/Components/Styles/CustomSlider.jsx
@@ -0,0 +1,107 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Box, Slider, Typography, TextField } from "@mui/material";
+
+const CustomSlider = ({
+  value,
+  onChange,
+  label,
+  min = 0,
+  max = 1,
+  step = 0.01,
+  onInputChange,
+}) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        gap: "5px",
+        marginBottom: "10px",
+        marginRight: "5px",
+      }}
+    >
+      <Typography
+        sx={{
+          fontSize: "12px",
+          fontWeight: "normal",
+          color: "#282828",
+          marginRight: "10px",
+          marginLeft: "20px",
+          fontFamily: "Avenir, sans-serif",
+          textAlign: "left",
+        }}
+      >
+        {label}
+      </Typography>
+
+      {/* Slider */}
+      <Slider
+        value={value}
+        min={min}
+        max={max}
+        step={step}
+        onChange={onChange}
+        track="inverted"
+        sx={{
+          width: "120px",
+          height: "20px",
+          padding: "0px",
+          "& .MuiSlider-thumb": {
+            width: "1px",
+            height: "25px",
+            backgroundColor: "green",
+            "&:focus, &:hover, &:active, &:focus-visible": {
+              boxShadow: "none",
+              outline: "none",
+              border: "none",
+            },
+            "&:before": {
+              display: "none",
+            },
+          },
+          "& .MuiSlider-track": {
+            backgroundColor: "transparent",
+            height: "100%",
+            borderRadius: "2px",
+          },
+          "& .MuiSlider-rail": {
+            backgroundColor: "#e0e0e0",
+            height: "100%",
+            borderRadius: "2px",
+          },
+        }}
+      />
+
+      {/* TextField for manual value input */}
+      <Box sx={{ paddingLeft: "7px" }}>
+        <TextField
+          value={value.toFixed(2)}
+          onChange={onInputChange}
+          sx={{
+            width: "60px",
+            "& .MuiInputBase-root": {
+              height: "24px",
+              padding: "0px",
+              fontSize: "12px",
+              textAlign: "center",
+              color: "#529d36",
+              backgroundColor: "#ddd",
+              fontFamily: "Avenir, sans-serif",
+              fontWeight: "bold",
+            },
+            "& .MuiOutlinedInput-notchedOutline": {
+              border: "none",
+            },
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default CustomSlider;
+//v3 - All UI updated
+//v4- Ui with figma
